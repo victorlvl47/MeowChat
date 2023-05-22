e3e5300e73e3d1522da15be5b9c7ab4905e6dc37
victor <victorlvl47@gmail.com>
2023-05-22 14:00:24 -0600

    more cat related styles

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,7 +24,7 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <title>MeowChat</title>
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
