c9a3c7e6a76ceffc35d998d2370aecb03bf3dcee
victor <victorlvl47@gmail.com>
2023-05-27 22:25:33 -0600

    in manage posts, redirect to show post if the user click on the title of the post

--- resources/views/posts/manage.blade.php
+++ resources/views/posts/manage.blade.php
@@ -16,7 +16,7 @@
                     <td
                         class="px-4 py-8 border-t border-b border-gray-300 text-lg"
                     >
-                        <a href="show.html">
+                        <a href="/posts/{{$post->id}}">
                             {{$post->title}}
                         </a>
                     </td>
